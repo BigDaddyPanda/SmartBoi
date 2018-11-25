c83a031357e97a2f2406de9596a6364bbfa495e4
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2018-11-25 19:26:39 +0100

    clearing code

--- Prototype/WebApp/Word2VecProto/scripts/word2vec.py
+++ Prototype/WebApp/Word2VecProto/scripts/word2vec.py
@@ -60,72 +60,10 @@ def evaluate_sentence(sentence, ref_model):
         ls = [i for i in sentence if i != word and len(i) > 2]
         try:
             sims = ref_model.wv.similarity(ls)
-        except:
+        except Exception as E:
             sims = [('', 0)]
         result.append(sum([t[1] for t in sims]) / len(sims))
 
     return sum(result) / len(result)
 
 
-if __name__ == '__main__':
-    # save only the word vectors
-    model = train_prototype_model()
-
-    w1 = "dirty"
-    print("Most similar to {0}".format(w1), model.wv.most_similar(positive=w1))
-
-    # look up top 6 words similar to 'polite'
-    w1 = ["polite"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # look up top 6 words similar to 'france'
-    w1 = ["france"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # look up top 6 words similar to 'shocked'
-    w1 = ["shocked"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # look up top 6 words similar to 'shocked'
-    w1 = ["beautiful"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # get everything related to stuff on the bed
-    w1 = ["bed", 'sheet', 'pillow']
-    w2 = ['couch']
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            negative=w2,
-            topn=10))
-
-    # similarity between two different words
-    print("Similarity between 'dirty' and 'smelly'",
-          model.wv.similarity(w1="dirty", w2="smelly"))
-
-    # print(model.wv.cosine_similarities(["dirty"],["clothes"]))
-
-    # similarity between two identical words
-    print("Similarity between 'dirty' and 'dirty'",
-          model.wv.similarity(w1="dirty", w2="dirty"))
-
-    # similarity between two unrelated words
-    print("Similarity between 'dirty' and 'clean'",
-          model.wv.similarity(w1="dirty", w2="clean"))
